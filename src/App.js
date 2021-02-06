07d3d1e490c12ebe0c5aa72a89ac40abd49a37e7
mazarafa <arruda.rafa.ra@gmail.com>
2021-02-06 15:28:10 -0400

    improving data visualization, making up with css 😅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react';
 import {  Title} from "./components/Title";
-import {  SearchForm} from "./components/SearchForm";
+import { SearchForm} from "./components/SearchForm";
+import { SearchList} from "./components/SearchList";
 
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -13,12 +14,20 @@ class App extends Component {
     this.setState({results})
   }
 
-  _renderResults(){
-    const{ results} = this.state
-    return results.map(movie =>{
-      return <p key={movie.imdbID}>{movie.Title}</p>
-    })
-  }
+  /*   _renderResults(){
+      const{ results} = this.state
+      return results.map(movie =>{
+        return(
+
+          <Movie 
+            key={movie.imdbID}
+            title = {movie.Title}
+            year = {movie.Year}
+            poster = {movie.Poster}
+          />
+        )
+      })
+  } */
   
   render(){
     return (
@@ -30,7 +39,8 @@ class App extends Component {
         
         { this.state.results.length === 0 
           ? <p>Sin resultados</p>
-          : this._renderResults()
+          //: this._renderResults()
+          :<SearchList movies = {this.state.results}/>
         }
        
       </div>
---
+++ src/components/SearchList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Movie } from './Movie'
+
+export class SearchList extends Component {
+  static propTypes = {
+    movies: PropTypes.array
+  }
+
+  render () {
+    const { movies } = this.props
+    return (
+      <div className='SearchList'>
+        {
+          movies.map(movie => {
+            return (
+              <div key={movie.imdbID} className='SearchList-item'>
+                <Movie
+                  id={movie.imdbID}
+                  title={movie.Title}
+                  year={movie.Year}
+                  poster={movie.Poster}
+                />
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
