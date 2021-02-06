86a3b71239dc3e01e28e9c02e95788404d4b1128
mazarafa <arruda.rafa.ra@gmail.com>
2021-02-06 14:50:32 -0400

    interacting an API in the application🤡

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React,{ Component } from 'react';
+import {  Title} from "./components/Title";
+import {  SearchForm} from "./components/SearchForm";
 
 import './App.css';
-import { Component } from 'react';
 import 'bulma/css/bulma.css';
-import {  Title} from "./components/Title";
+
 
 class App extends Component {
+  state = { results:[]}
+
+  _handleResults = (results) => {
+    this.setState({results})
+  }
+
+  _renderResults(){
+    const{ results} = this.state
+    return results.map(movie =>{
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+  
   render(){
     return (
       <div className="App">
         <Title>NETFLIX</Title>
+        <div className="searchForm-wrapper">
+          <SearchForm onResults={this._handleResults}/>
+        </div>
+        
+        { this.state.results.length === 0 
+          ? <p>Sin resultados</p>
+          : this._renderResults()
+        }
        
       </div>
     );
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export const Title = ({children})=>(
-    <h1 class="title">{children}</h1>
+    <h1 className="title">{children}</h1>
 )
 
 /* export default ({children}) => (
